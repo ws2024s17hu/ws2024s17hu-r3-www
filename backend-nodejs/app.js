4f35ce820fa118c6adaf9bfe42861712497174cd
Botond Sisak <sisak.botond@gmail.com>
2023-04-21 23:41:15 +0200

    new folder strcuture

--- backend-nodejs/app.js
+++ backend-nodejs/app.js
@@ -1,10 +1,8 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("<h1>Backend (Node.js)</h1>");
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end("<h1>Backend (Node.js)</h1>");
 });
- 
 server.listen(80, () => {
-  console.log(`Server is running`);
+  console.log(`Server is running`);
 });
-
